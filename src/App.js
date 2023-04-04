6a5ae487417fc52592a2fbcb57c6a3b4927a5c47
Smarthug <kirklayer@gmail.com>
2023-04-04 06:59:18 +0000

    changed name

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactfefeffefef
+            Learn CodeSpacekkfjie
           </a>
         </p>
       </header>
